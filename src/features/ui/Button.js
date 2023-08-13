9e3d216123723a57b98831b2441d3910b6b4d801
YagnikAkbari2 <yagnik_20003@ldrp.ac.in>
2023-08-13 15:41:35 +0530

    add dynamic width to button

--- src/features/ui/Button.js
+++ src/features/ui/Button.js
@@ -8,14 +8,15 @@ const Button = ({
   id,
   type = "submit",
   typeName = "primary",
+  width = "w-full",
 }) => {
-  const classes = `${className} bg-primary-700 w-full p-[0.8rem] text-white rounded-[0.4rem] disabled:cursor-not-allowed disabled:bg-stone-300`;
+  const classes = `${className} bg-primary-700 ${width} p-[0.8rem] text-white rounded-[0.4rem] disabled:cursor-not-allowed disabled:bg-stone-300`;
   if (typeName === "secondary") {
     return (
       <button
         type={type}
         id={id}
-        className={`${className} bg-transparent px-5 text-purple-700 border-primary-700 border-2 rounded-[4px] p-[1rem]`}
+        className={`${className} bg-transparent px-5 text-purple-700 border-primary-700 border-2 rounded-[0.4rem] p-[10.8px] box-content`}
         disabled={disabled}
         onClick={onClick}
       >
