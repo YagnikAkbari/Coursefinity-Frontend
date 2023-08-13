fca476fbff60150b6e004255e66b0df2f93f4611
YagnikAkbari2 <yagnik_20003@ldrp.ac.in>
2023-08-13 16:09:53 +0530

    change the logic for viewing all the course from state to another link

--- src/features/Home/LearnerHome.js
+++ src/features/Home/LearnerHome.js
@@ -1,36 +1,24 @@
-import { useState } from "react";
 import CourseItem from "../course/components/CourseItem";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 function LearnerHome() {
-  const [viewAllCourse, setViewAllCourse] = useState(false);
   const data = useLoaderData();
   const courseList = data.body.message;
-  const displayAllCourseHandler = () => {
-    setViewAllCourse((prevState) => !prevState);
-  };
 
   return (
     <>
       <h1 className="space-x-3 text-xl font-semibold mx-24 mt-10">
         <span>Continue your learning Courses</span>
-        <button className="text-primary-700" onClick={displayAllCourseHandler}>
-          {!viewAllCourse ? "View all" : "hide"}
-        </button>
+        <Link className="text-primary-700" to="/my-courses">
+          View all
+        </Link>
       </h1>
       <div
-        className={`text-center grid w-full pt-5 pb-8 px-[6rem] gap-y-5 ${
-          viewAllCourse ? "grid-cols-2 gap-y-10" : "grid-cols-4"
-        }`}
+        className={`text-center grid w-full pt-5 pb-8 px-[6rem] gap-y-5 grid-cols-4`}
       >
-        {!viewAllCourse &&
-          courseList.map((course) => {
-            return <CourseItem course={course} key={course._id} />;
-          })}
-        {viewAllCourse &&
-          courseList.map((course) => {
-            return <CourseItem course={course} key={course.id} view="all" />;
-          })}
+        {courseList.map((course) => {
+          return <CourseItem course={course} key={course._id} />;
+        })}
       </div>
     </>
   );
