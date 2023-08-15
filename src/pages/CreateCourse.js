caa82df8fae2f3bbb43c073832ce01ad44ca30d6
YagnikAkbari2 <yagnik_20003@ldrp.ac.in>
2023-08-15 10:40:47 +0530

    add ui component uploadmodal and it's styling with backdrop styling and change the component createCourseModule to open modal for upload assignment

--- src/pages/CreateCourse.js
+++ src/pages/CreateCourse.js
@@ -1,61 +1,19 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import useForm from "../features/auth/form-hook";
-import { VALIDATOR_REQUIRE } from "../utils/validators";
-import Input from "../features/ui/input";
-import Button from "../features/ui/Button";
+import CreateCourses from "../features/createCourse/CreateCourses";
+import { Helmet } from "react-helmet";
 
 const CreateCoursePage = () => {
-  const navigate = useNavigate();
-  const [formState, inputHandler] = useForm(
-    {
-      title: {
-        value: "",
-        isValid: false,
-      },
-      description: {
-        value: "",
-        isValid: false,
-      },
-    },
-    false
-  );
-
-  const handleSubmit = async (event) => {
-    navigate("/create-module");
-  };
-
   return (
-    <div className="flex flex-col m-auto mt-5 w-[24rem]">
-      <h2 className="font-bold text-[1.5rem] leading-[1.8rem] text-center">
-        Create your Course
-      </h2>
-
-      <form method="post" onSubmit={handleSubmit}>
-        <Input
-          id="title"
-          type="text"
-          element="input"
-          placeholder="Enter course name"
-          validators={[VALIDATOR_REQUIRE()]}
-          onInput={inputHandler}
-          errorText="Please enter a valid course name."
-          className="w-full relative rounded-[0.4rem] p-[0.8rem] mt-[2rem] border-0 focus:ring-0 ml-auto"
+    <>
+      <Helmet>
+        <title>Create Course | coursefinity</title>
+        <meta
+          name="description"
+          content="coursefinity are here to meet indian educators with the student from every corner of india"
         />
-        <Input
-          id="description"
-          element="textarea"
-          placeholder="Enter course description"
-          validators={[VALIDATOR_REQUIRE()]}
-          onInput={inputHandler}
-          errorText="Please enter a valid course description."
-          className="w-full relative  rounded-[0.4rem] p-[0.8rem] mt-[1.5rem] border-0 focus:ring-0"
-        />
-        <Button type="submit" className="mt-10" disabled={!formState.isValid}>
-          Next
-        </Button>
-      </form>
-    </div>
+      </Helmet>
+      <CreateCourses />
+    </>
   );
 };
 
